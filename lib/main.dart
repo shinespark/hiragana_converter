a5e0f9fd5b9d8a1dfba2c61f5e3ed655cedfcb50
shinespark <shnsprk@gmail.com>
2024-11-06 01:33:07 +0900

    8.8 画面の切り替えを行う

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:hiragana_converter/app_state.dart';
+import 'package:hiragana_converter/app_notifier_provider.dart';
+import 'package:hiragana_converter/convert_result.dart';
 import 'package:hiragana_converter/input_form.dart';
+import 'package:hiragana_converter/loading_indicator.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -22,17 +29,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appState = ref.watch(appNotifierProvider);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Hiragana Converter'),
       ),
-      body: const InputForm(),
+      body: switch (appState) {
+        Loading() => const LoadingIndicator(),
+        Input() => const InputForm(),
+        Data(sentence: final sentence) => ConvertResult(sentence: sentence),
+      }
     );
   }
 }
